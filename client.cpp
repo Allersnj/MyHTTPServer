3286b6e107ec6ee2fe9f41daceede3e1287d6f06
Nathan Allers <nateallers@gmail.com>
2020-09-03 00:40:20 -0500

    Finished chapter 7 goals

--- client.cpp
+++ client.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cassert>
+#include <vector>
 #include <string>
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -27,6 +28,33 @@ int test_command(SOCKET ConnectSocket, std::string_view command, std::string_vie
 	
 	testResponse = recvbuf;
 	assert(testResponse == response);
+	return iResult;
+}
+
+int test_command(SOCKET ConnectSocket, std::string_view command, const std::vector<std::string_view>& responses)
+{
+	char recvbuf[DEFAULT_BUFLEN];
+	std::string testResponse;
+	int iResult = send(ConnectSocket, command.data(), command.length(), 0);
+	if (iResult == SOCKET_ERROR)
+	{
+		std::cout << "send failed with error: " << WSAGetLastError() << '\n';
+		return iResult;
+	}
+	for (auto response : responses)
+	{
+		iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN, 0);
+		if (iResult < 0)
+		{
+			std::cout << "recv failed with error: " << WSAGetLastError() << '\n';
+			return iResult;
+		}
+		recvbuf[iResult] = 0;
+	
+		testResponse = recvbuf;
+		assert(testResponse == response);
+	}
+	
 	return iResult;
 }
 
@@ -88,7 +116,8 @@ int main()
 	}
 	
 	// Run tests here
-	iResult = test_command(ConnectSocket, "GET red", "ANSWER A color\r\n");
+	
+	iResult = test_command(ConnectSocket, "GET red\n", "ANSWER A color\r\n");
 	if (iResult == SOCKET_ERROR)
 	{
 		closesocket(ConnectSocket);
@@ -96,8 +125,63 @@ int main()
 		return 1;
 	}
 	
-	iResult = test_command(ConnectSocket, "ALL", 
+	iResult = test_command(ConnectSocket, "ALL\n", {"puppy: A young dog\r\n", "red: A color\r\n"});
+	if (iResult == SOCKET_ERROR)
+	{
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
 	
+	iResult = send(ConnectSocket, "CLEAR\n", 6, 0);
+	if (iResult == SOCKET_ERROR)
+	{
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	iResult = test_command(ConnectSocket, "ALL\n", "Dictionary is empty\r\n");
+	if (iResult == SOCKET_ERROR)
+	{
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	iResult = send(ConnectSocket, "SET blue Another color\n", 23, 0);
+	if (iResult == SOCKET_ERROR)
+	{
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	iResult = test_command(ConnectSocket, "GET blue\n", "ANSWER Another color\r\n");
+	if (iResult == SOCKET_ERROR)
+	{
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	iResult = test_command(ConnectSocket, "blah\n", "ERROR unknown command\r\n");
+	if (iResult == SOCKET_ERROR)
+	{
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	iResult = test_command(ConnectSocket, "QUIT\n", "Goodbye!\r\n");
+	if (iResult == SOCKET_ERROR)
+	{
+		closesocket(ConnectSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	std::cout << "All tests passed.\n";
 	
 	iResult = shutdown(ConnectSocket, SD_SEND);
 	if (iResult == SOCKET_ERROR)
--- main.cpp
+++ main.cpp
@@ -8,12 +8,6 @@
 #define DEFAULT_PORT "27015"
 #define DEFAULT_BUFLEN 512
 
-int init()
-{
-	WSADATA wsaData;
-	return WSAStartup(MAKEWORD(2,2), &wsaData);
-}
-
 SOCKET getListenSocket()
 {
 	struct addrinfo *result = NULL, hints;
@@ -68,7 +62,8 @@ int main()
 	dictionary["red"] = "A color";
 	dictionary["puppy"] = "A young dog";
 	
-	int iResult = init();
+	WSADATA wsaData;
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
 	if (iResult != 0)
 	{
 		std::cout << "WSAStartup failed: " << iResult << '\n';
@@ -99,77 +94,104 @@ int main()
 			std::cout << "Bytes received: " << iResult << '\n';
 			recvbuf[iResult] = 0; // Always null-terminate your strings.
 			std::stringstream stream{recvbuf};
-			stream >> command;
-			if (command == "GET")
+			while (stream >> command)
 			{
-				stream >> command;
-				if (dictionary.count(command))
+				if (command == "GET")
 				{
-					resultStr = "ANSWER " + dictionary[command] + "\r\n";
+					stream >> command;
+					if (dictionary.count(command))
+					{
+						resultStr = "ANSWER " + dictionary[command] + "\r\n";
+					}
+					else
+					{
+						resultStr = "ERROR can't find " + command + "\r\n";
+					}
+					iSendResult = send(ClientSocket, resultStr.c_str(), resultStr.length(), 0);
+					if (iSendResult == SOCKET_ERROR)
+					{
+						std::cout << "send failed: " << WSAGetLastError() << '\n';
+						closesocket(ClientSocket);
+						WSACleanup();
+						return 1;
+					}
+					std::cout << "Bytes sent: " << iSendResult << '\n';
+				}
+				else if (command == "SET")
+				{
+					stream >> command;
+					stream.get();
+					stream.getline(recvbuf, DEFAULT_BUFLEN, '\n');
+					dictionary[command] = recvbuf;
+				}
+				else if (command == "CLEAR")
+				{
+					dictionary.clear();
+				}
+				else if (command == "ALL")
+				{
+					if (dictionary.size() == 0)
+					{
+						resultStr = "Dictionary is empty\r\n";
+						iSendResult = send(ClientSocket, resultStr.c_str(), resultStr.length(), 0);
+						if (iSendResult == SOCKET_ERROR)
+						{
+							std::cout << "send failed: " << WSAGetLastError() << '\n';
+							closesocket(ClientSocket);
+							WSACleanup();
+							return 1;
+						}
+						std::cout << "Bytes sent: " << iSendResult << '\n';
+					}
+					else
+					{
+						for (const auto& [key, value] : dictionary)
+						{
+							resultStr = key + ": " + value + "\r\n";
+							iSendResult = send(ClientSocket, resultStr.c_str(), resultStr.length(), 0);
+							if (iSendResult == SOCKET_ERROR)
+							{
+								std::cout << "send failed: " << WSAGetLastError() << '\n';
+								closesocket(ClientSocket);
+								WSACleanup();
+								return 1;
+							}
+							std::cout << "Bytes sent: " << iSendResult << '\n';
+						}
+					}
+				}
+				else if (command == "")
+				{
+					continue;
+				}
+				else if (command == "QUIT")
+				{
+					resultStr = "Goodbye!\r\n";
+					iSendResult = send(ClientSocket, resultStr.c_str(), resultStr.length(), 0);
+					if (iSendResult == SOCKET_ERROR)
+					{
+						std::cout << "send failed: " << WSAGetLastError() << '\n';
+						closesocket(ClientSocket);
+						WSACleanup();
+						return 1;
+					}
+					break;
 				}
 				else
 				{
-					resultStr = "ERROR can't find " + command + "\r\n";
-				}
-				iSendResult = send(ClientSocket, resultStr.c_str(), resultStr.length(), 0);
-			    if (iSendResult == SOCKET_ERROR)
-			    {
-				    std::cout << "send failed: " << WSAGetLastError() << '\n';
-				    closesocket(ClientSocket);
-				    WSACleanup();
-				    return 1;
-			    }
-			    std::cout << "Bytes sent: " << iSendResult << '\n';
-			}
-			else if (command == "SET")
-			{
-				stream >> command;
-				stream.get();
-				stream.getline(recvbuf, DEFAULT_BUFLEN, 0);
-				dictionary[command] = recvbuf;
-			}
-			else if (command == "CLEAR")
-			{
-				dictionary.clear();
-			}
-			else if (command == "ALL")
-			{
-				for (const auto& [key, value] : dictionary)
-				{
-					resultStr = key + ": " + value + "\r\n";
+					resultStr = "ERROR unknown command\r\n";
 					iSendResult = send(ClientSocket, resultStr.c_str(), resultStr.length(), 0);
-			        if (iSendResult == SOCKET_ERROR)
-			        {
-				        std::cout << "send failed: " << WSAGetLastError() << '\n';
-				        closesocket(ClientSocket);
-				        WSACleanup();
-				        return 1;
-			        }
-			        std::cout << "Bytes sent: " << iSendResult << '\n';
+					if (iSendResult == SOCKET_ERROR)
+					{
+						std::cout << "send failed: " << WSAGetLastError() << '\n';
+						closesocket(ClientSocket);
+						WSACleanup();
+						return 1;
+					}
+					std::cout << "Bytes sent: " << iSendResult << '\n';
 				}
+				command = "";
 			}
-			else if (command == "")
-			{
-				//std::cout << "Ignoring empty command...\n";
-			}
-			else if (command == "QUIT")
-			{
-				break;
-			}
-			else
-			{
-				resultStr = "ERROR unknown command\r\n";
-				iSendResult = send(ClientSocket, resultStr.c_str(), resultStr.length(), 0);
-			    if (iSendResult == SOCKET_ERROR)
-			    {
-				    std::cout << "send failed: " << WSAGetLastError() << '\n';
-				    closesocket(ClientSocket);
-				    WSACleanup();
-				    return 1;
-			    }
-			    std::cout << "Bytes sent: " << iSendResult << '\n';
-			}
-			command = "";
 		}
 		else if (iResult == 0)
 		{
@@ -196,6 +218,4 @@ int main()
 	
 	closesocket(ClientSocket);
 	WSACleanup();
-	
-	std::cin.get();
 }
